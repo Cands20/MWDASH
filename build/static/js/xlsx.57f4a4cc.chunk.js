6c7a2804953f1fe84addd831069c46b53c3616e5
Cesar Sanchez <sanchez.cesar95@gmail.com>
2021-12-21 12:52:09 -0600

    panel, perdon juanito :c

---
+++ build/static/js/xlsx.57f4a4cc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpeasydev=this.webpackJsonpeasydev||[]).push([[2],{1973:function(n,s){},2027:function(n,s){},2028:function(n,s){}}]);
+//# sourceMappingURL=xlsx.57f4a4cc.chunk.js.map
